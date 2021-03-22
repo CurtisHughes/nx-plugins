e009d662f657aad7022041bba6ef22f536f721cf
Curtis Hughes <curtis@curtiss-mbp.attlocal.net>
2021-03-21 23:01:04 -0500

    feat: rollup plugin

--- packages/nx-plugin-rollup/src/executors/build/executor.ts
+++ packages/nx-plugin-rollup/src/executors/build/executor.ts
@@ -1,8 +1,33 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import rollup = require('rollup');
+import typescript = require('@rollup/plugin-typescript');
+import { resolve } from 'path';
 import { BuildExecutorSchema } from './schema';
 
-export default async function runExecutor(options: BuildExecutorSchema) {
-  console.log('Executor ran for Build', options);
-  return {
-    success: true,
+export default async function (options: BuildExecutorSchema) {
+  const defaultConfig: rollup.RollupOptions = {
+    input: resolve(process.cwd(), options.input),
+    plugins: [
+      (typescript as any)({
+        tsconfig: resolve(process.cwd(), options.tsConfig),
+      }),
+    ],
   };
+
+  const config: rollup.RollupOptions = require(resolve(
+    process.cwd(),
+    options.rollupConfig
+  ))(defaultConfig);
+
+  const bundle = await rollup.rollup(config);
+
+  const outputOptions = Array.isArray(config.output)
+    ? config.output
+    : [config.output];
+
+  await Promise.all(
+    (<Array<rollup.OutputOptions>>outputOptions).map((o) => bundle.write(o))
+  );
+
+  return { success: true };
 }
--- packages/nx-plugin-rollup/src/executors/build/schema.d.ts
+++ packages/nx-plugin-rollup/src/executors/build/schema.d.ts
@@ -1,1 +1,5 @@
-export interface BuildExecutorSchema {} // eslint-disable-line
+export interface BuildExecutorSchema {
+  input: string;
+  rollupConfig: string;
+  tsConfig: string;
+}
